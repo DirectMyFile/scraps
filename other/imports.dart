c31b48554fce6afe8dcb0cf60666012ad727ad8f
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-04 00:26:06 -0400

    Add Resource Gen and Imports Rewriter

---
+++ other/imports.dart
@@ -0,0 +1,39 @@
+import "package:analyzer/analyzer.dart";
+import "package:analyzer/src/generated/scanner.dart";
+
+void rewriteImports(CompilationUnit cu, String rewrite(String uri)) {
+  List<ImportDirective> imports = cu.directives.where((it) => it is ImportDirective).toList();
+
+  for (var import in imports) {
+    var v = rewrite(import.uri.stringValue);
+    import.uri = new SimpleStringLiteral(new StringToken(TokenType.STRING, '"' + v + '"', 0), v);
+  }
+}
+
+const String INPUT = """
+import "github:DirectMyFile/scraps/tools/which.dart" as which;
+""";
+
+void main() {
+  var unit = parseCompilationUnit(INPUT);
+  rewriteImports(unit, (url) {
+    if (url.startsWith("github:")) {
+      var uri = Uri.parse(url);
+
+      if (uri.pathSegments.length < 3) {
+        throw new Exception("Failed to rewrite GitHub Import. Invalid!");
+      }
+
+      var slug = uri.pathSegments.take(2).join("/");
+      var path = uri.pathSegments.skip(2).join("/");
+      var ref = "HEAD";
+      if (uri.hasFragment && uri.fragment.isNotEmpty) {
+        ref = uri.fragment;
+      }
+      return "https://raw.githubusercontent.com/${slug}/${ref}/${path}";
+    }
+
+    return url;
+  });
+  print(unit.toSource());
+}
---
+++ other/resource_gen.dart
@@ -0,0 +1,31 @@
+import "dart:io";
+
+String generate(Map<String, String> resources) {
+  var c = "const Map<String, String> resources = const {";
+  if (resources.isNotEmpty) {
+    c += "\n  ";
+  }
+  c += resources.keys.map((String name) {
+    return '"${name}": r"""${resources[name]}"""';
+  }).join(",\n");
+  if (resources.isNotEmpty) {
+    c += "\n";
+  }
+  c += "};";
+  return c;
+}
+
+String generateDirectory(String path) {
+  var dir = new Directory(path);
+  var map = {};
+  dir.listSync(recursive: true).where((it) => it is File).map((it) {
+    return [it.path.replaceAll(dir.path + Platform.pathSeparator, ""), it.readAsStringSync()];
+  }).forEach((x) {
+    map[x[0]] = x[1];
+  });
+  return generate(map);
+}
+
+void main() {
+  print(generateDirectory("../numbers"));
+}
